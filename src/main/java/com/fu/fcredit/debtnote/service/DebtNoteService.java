37c8f7fe597c7c5fa2559c24813026e73ecaaa54
antha <ngothanhan2352002@gmail.com>
2023-03-20 23:18:05 +0700

    update get debt note

--- src/main/java/com/fu/fcredit/debtnote/service/DebtNoteService.java
+++ src/main/java/com/fu/fcredit/debtnote/service/DebtNoteService.java
@@ -3,15 +3,18 @@ package com.fu.fcredit.debtnote.service;
 import com.fu.fcredit.debtnote.entity.DebtNote;
 import com.fu.fcredit.debtnote.repository.DebtNoteRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class DebtNoteService {
+    @Autowired
     private final DebtNoteRepository repository;
 
     public Page<DebtNote> getDebtDetail(Pageable pageable) {
@@ -22,6 +25,11 @@ public class DebtNoteService {
         debtNote.setCreateDate(new Date());
 
         return repository.save(debtNote);
+    }
+    public DebtNote getDebtNoteById(Long id){
+        Optional<DebtNote> debtorNote = repository.findById(id);
+
+        return debtorNote.get();
     }
 
 
